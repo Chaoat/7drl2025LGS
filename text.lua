2b870c7307c50d3e49880e7d08ffcfdc1e299521
Reuben Covington <reubencovington@gmail.com>
2025-03-07 22:30:31 +1100

    bunker descriptions

--- text.lua
+++ text.lua
@@ -13,55 +13,55 @@ do
 
 	--Bunker TEXT
 	textEntries["bunker1"] = "South Street"
-	textEntries["bunker1description"] = "TODO"
+	textEntries["bunker1description"] = "Your boat drifts into the pier, the creaking wood barely holding against the rising tide.\nThe Bunker stands firm against the flood, its walls weathered but unbroken.\nWithin, survivors move with quiet purpose, bartering, repairing, preparing for another day in the drowned world.\nSafe for now, but never secure."
 
 	textEntries["bunker2"] = "North West Cafe"
-	textEntries["bunker2description"] = "TODO"
+	textEntries["bunker2description"] = "Perched precariously on a small hill, this outpost is a beacon of warmth and life in this watery wasteland. The weathered sign above the entrance reads The Last Sip. You're greeted by a warm, inviting atmosphere. The air is thick with the aroma of freshly brewed coffee and a homely fireplace. Stop, rest, refuel before facing the crazy world out there again."
 
 	textEntries["bunker3"] = "Collapsed Trade Tower"
 	textEntries["bunker3description"] = "The skeletal remains of the skyscraper loom above the waves, its steel ribs twisted and rusted from decades of neglect. What was once a beacon of commerce became a crumbling tomb, its lower floors swallowed by the sea, its upper levels tilting precariously against the wind.\nYet, against all odds, the tower once again breathes with purpose. The old offices, stripped of luxury, now serve as makeshift markets, where scavengers barter over wares.\nThe world may have collapsed, but commerce endures. It always does."
 
 	textEntries["bunker4"] = "Warehouse Depot"
-	textEntries["bunker4description"] = "TODO"
+	textEntries["bunker4description"] = "Rusted cranes tower above the water, silent sentinels of a bygone world.\nYour boat glides up to the dock, past half-sunken cargo containers and debris-littered piers.\nInside the warehouse, workers sort scavenged goods, marking crates, and shouting across the cavernous space."
 
 	textEntries["bunker5"] = "Destroyed Warehouse"
-	textEntries["bunker5description"] = "TODO"
+	textEntries["bunker5description"] = "The Bunker stands firm against the tide, its walls weathered but unbroken. Within, survivors barter, repair, and prepare for another day in the drowned world. Safe for now, but never secure."
 
 	textEntries["bunker6"] = "Mountain Cabin"
-	textEntries["bunker6description"] = "TODO"
+	textEntries["bunker6description"] = "The Bunker stands firm against the tide, its walls weathered but unbroken. Within, survivors barter, repair, and prepare for another day in the drowned world. Safe for now, but never secure."
 
 	textEntries["bunker7"] = "North Central Plaza"
-	textEntries["bunker7description"] = "TODO"
+	textEntries["bunker7description"] = "The Bunker stands firm against the tide, its walls weathered but unbroken. Within, survivors barter, repair, and prepare for another day in the drowned world. Safe for now, but never secure."
 
 	textEntries["bunker8"] = "Central Offices"
-	textEntries["bunker8description"] = "TODO"
+	textEntries["bunker8description"] = "The Bunker stands firm against the tide, its walls weathered but unbroken. Within, survivors barter, repair, and prepare for another day in the drowned world. Safe for now, but never secure."
 
 	textEntries["bunker9"] = "Mountain Hotel"
-	textEntries["bunker9description"] = "TODO"
+	textEntries["bunker9description"] = "Your vessel approaches a rocky outcrop, the sheer cliff face rising above the waterline. A fortified bunker, clings to the mountainside like a tenacious vine. The floodwaters lap against its lower levels, but the structure stands resolute, a testament to the patchwork engineering carried out upon what might of once been a hotel."
 
 	textEntries["bunker10"] = "State Library"
-	textEntries["bunker10description"] = "TODO"
+	textEntries["bunker10description"] = "Majesty still exists in this drowned city at the last library left in the known world. The marble columns shimmer with light from the water below as your boat approaches the pier. One day, some hope the knowledge contained here can bootstrap a new civilization from the wreckage. However, survivors cannot eat books, so other priorities come first. "
 
 	textEntries["bunker11"] = "Warehouse Gatehouse"
-	textEntries["bunker11description"] = "TODO"
+	textEntries["bunker11description"] = "The Bunker stands firm against the tide, its walls weathered but unbroken. Within, survivors barter, repair, and prepare for another day in the drowned world. Safe for now, but never secure."
 
 	textEntries["bunker12"] = "Survivor Trade Hub"
-	textEntries["bunker12description"] = "TODO"
+	textEntries["bunker12description"] = "The Bunker stands firm against the tide, its walls weathered but unbroken. Within, survivors barter, repair, and prepare for another day in the drowned world. Safe for now, but never secure."
 
 	textEntries["bunker13"] = "Stadium"
-	textEntries["bunker13description"] = ""
+	textEntries["bunker13description"] = "The roar of the crowd lingers in this monument to sport. The stadium's field has become a shallow flooded field used to grow the rice that feeds much of the surrounding settlements. You've heard rumors that at night smugglers ship out crates of rice wine as a side business."
 
 	textEntries["bunker14"] = "North East Mall"
-	textEntries["bunker14description"] = ""
+	textEntries["bunker14description"] = "The shattered facade of the once-grand shopping mall yawns before you, a concrete maw swallowing the ragged remnants of humanity. The faded shopfronts now house refugees from other cities, trying to scour together enough to make their dreams of a better world a possibility."
 
 	textEntries["bunker15"] = "Untouched Tower"
-	textEntries["bunker15description"] = ""
+	textEntries["bunker15description"] = "Your boat glides alongside a small office building that stands almost untouched by the devastation. The floodwaters lap gently against its base, but the reinforced glass and sturdy concrete show no signs of yielding. It’s a stark anomaly in this drowned cityscape, a relic of the old world stubbornly clinging to existence."
 
 	textEntries["bunker16"] = "Southern Hideout"
-	textEntries["bunker16description"] = ""
+	textEntries["bunker16description"] = "The Bunker stands firm against the tide, its walls weathered but unbroken. Within, survivors barter, repair, and prepare for another day in the drowned world. Safe for now, but never secure."
 
 	textEntries["bunker17"] = "Outskirts Lookout"
-	textEntries["bunker17description"] = ""
+	textEntries["bunker17description"] = "The Bunker stands firm against the tide, its walls weathered but unbroken. Within, survivors barter, repair, and prepare for another day in the drowned world. Safe for now, but never secure."
 
 	textEntries["bunkerCollapsedDescription"] = "The Bunker stands silent. Only the wind answers your arrival, whistling through empty corridors and shattered gates.\nWhat drove them away? Monsters, famine, mutiny? You cannot say.\nHad you arrived sooner, perhaps things would be different. Perhaps this place would still have voices to greet you.\n\nNothing useful remains, you feel you best depart quickly."
 end
